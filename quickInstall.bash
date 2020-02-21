fa462c0031c16b9428f6d5fd2e475fc4426fd53f
Luke Wilson <l2wilson94@gmail.com>
2020-02-21 00:47:11 +0000

    Create quickInstall.bash

---
+++ quickInstall.bash
@@ -0,0 +1,35 @@
+# Requirements
+sudo apt-get install git make g++ cpanminus -y
+sudo apt-get install libsdl-image1.2-dev libsdl-ttf2.0-dev libcrypt-openssl-rsa-perl libcapture-tiny-perl -y
+sudo cpanm File::ShareDir
+
+# Files
+mkdir REPOS
+cd REPOS
+git clone https://github.com/DaveAckley/MFM.git
+git clone https://github.com/DaveAckley/ULAM.git
+git clone https://github.com/DaveAckley/SPLAT.git
+
+# MFM
+cd MFM
+make
+cd ..
+
+# ULAM
+cd ULAM
+make
+cd ..
+
+# SPLAT
+cd SPLAT
+perl Makefile.PL
+sudo make install
+cd ..
+
+# Keygen
+./MFM/bin/mfzmake keygen `whoami`
+./MFM/bin/mfzmake default `whoami`
+
+# Demo
+cd SPLAT/demos/FB
+make
